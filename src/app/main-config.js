cd2139f47161b88f31c51eb8d9b8d559d334945a
Cooper Thomas <cooperthomascooper@gmail.com>
2016-03-22 11:24:38 -0400

    Added Google analytics

--- src/app/main-config.js
+++ src/app/main-config.js
@@ -111,7 +111,7 @@ loadJS('app/main-app.js');
 // Enable Google Analytics on storymaps.esri.com
 if (window.location.href.toLowerCase().indexOf("storymaps.esri.com") >= 0) {
 	var _gaq = _gaq || [];
-	_gaq.push(['_setAccount', 'UA-26529417-1']);
+	_gaq.push(['_setAccount', 'UA-38806035-1']);
 	_gaq.push(['_trackPageview']);
 	
 	(function() {
@@ -119,4 +119,4 @@ if (window.location.href.toLowerCase().indexOf("storymaps.esri.com") >= 0) {
 		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 	})();
-}
+//}
